d3f61cb0378408d4767d21e9e14bd792ab892ba4
Christina Ford <fordc@uchicago.edu>
2018-08-21 11:20:17 -0400

    provided functionality for running on localhost

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def survey():
+    return render_template("form.html")
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+    return render_template("result.html", result=request.form)
