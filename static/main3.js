d394c9bf463386d5542fdb154053b382ee83a9c3
Christina Ford <fordc@cs.uchicago.edu>
2018-08-30 23:52:46 -0400

    updated test fills

--- static/main3.js
+++ static/main3.js
@@ -1,36 +1,58 @@
-function showDiv(name) {
-   document.getElementById(name).style.display = "block";
-}
-
 function fillForm() {
-  fill("firstname", "John");
-  fill("lastname", "Doe");
-  fill("email", "johndoe@example.com");
-  fill("phone", "123-456-7890");
-  fill("address1", "606-3727 Ullamcorper. Street");
-  fill("home_city", "Roseville");
-  fill("home_state", "NH");
-  fill("zip", "11523");
-  fill("country", "US");
+  document.getElementById("edu_add").click();
+  document.getElementById("exp_add").click();
+  document.getElementById("exp_add").click();
 
-  fill("school_name_1", "High School Name");
-  fill("level", "Secondary");
-  fill("school_city", "New York");
-  fill("school_state", "New York");
-  fill("grad_date1", "2018-08");
+  fill("personal_firstname_0", "John");
+  fill("personal_lastname_0", "Doe");
+  fill("personal_email_0", "johndoe@example.com");
+  fill("personal_number_0", "123-456-7890");
+  fill("personal_address1_0", "606-3727 Ullamcorper. Street");
+  fill("personal_address2_0", "address line 2");
+  fill("personal_city_0", "Roseville");
+  fill("personal_state_0", "NH");
+  fill("personal_zip_0", "11523");
+  fill("personal_country_0", "US");
 
-  fill("job_title", "Chief Executive Janitor");
-  fill("job_location", "Janitors Association Inc.");
-  fill("job_start1", "2018-08");
-  fill("job_end1", "2018-08");
-  fill("job_description", "The best janitor");
+  fill("edu_name_0", "High School Name");
+  fill("edu_level_0", "Secondary");
+  fill("edu_city_0", "New York");
+  fill("edu_state_0", "New York");
+  fill("edu_grad_date_0", "2018-08");
 
-  fill("activity_title", "Young Janitors of America");
-  fill("position_title", "President");
-  fill("activity_location", "College");
-  fill("activity_start1", "2018-08");
-  fill("activity_end1", "2018-08");
-  fill("activity_description", "I led young janitors");
+  fill("edu_name_1", "College Name");
+  fill("edu_level_1", "Bachelors");
+  fill("edu_city_1", "Chicago");
+  fill("edu_state_1", "Illinois");
+  fill("edu_grad_date_1", "2021-08");
+
+  fill("job_title_0", "Chief Executive Janitor");
+  fill("job_company_0", "Janitors Association Inc.");
+  fill("job_location_0", "Chicago, IL");
+  fill("job_start_date_0", "2018-08");
+  fill("job_end_date_0", "2018-08");
+  fill("job_description_0", "The best janitor");
+
+  fill("job_title_1", "Junior Janitor");
+  fill("job_company_1", "Janitors Association Inc.");
+  fill("job_location_1", "Evanston, IL");
+  fill("job_start_date_1", "2018-08");
+  fill("job_end_date_1", "2018-08");
+  fill("job_description_1", "The second best janitor");
+
+  fill("job_title_2", "Associate Janitor");
+  fill("job_company_2", "Janitors Association Inc.");
+  fill("job_location_2", "Ann Arbor, Michigan");
+  fill("job_start_date_2", "2018-08");
+  fill("job_end_date_2", "2018-08");
+  fill("job_description_2", "The most ok janitor");
+
+  fill("act_name_0", "Young Janitors of America");
+  fill("act_title_0", "President");
+  fill("act_location_0", "College");
+  fill("act_start_date_0", "2018-08");
+  fill("act_end_date_0", "2018-08");
+  fill("act_description_0", "I led young janitors");
 }
 
 function fill(id, val) {
