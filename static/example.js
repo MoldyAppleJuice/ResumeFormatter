d32727f5d4fd2562b9de14e7aab30f4cd7a6eb5b
Christina Ford <fordc@cs.uchicago.edu>
2018-08-31 00:10:25 -0400

    moved some things around

--- static/example.js
+++ static/example.js
@@ -1,3 +1,44 @@
+/* Global variables; objects representing the content of each module */
+var personal = {
+  "name":"personal",
+  "id_tags":["personal_firstname", "personal_lastname", "personal_email", "personal_number", "personal_address1", "personal_address2",
+                  "personal_city", "personal_state", "personal_zip", "personal_country"],
+  "labels":['First name* ', 'Last name* ', 'Email* ', 'Phone Number* ', 'Address Line 1* ',
+                'Address Line 2* ', 'City* ', 'State* ', 'Zip* ', 'Country* ',],
+  "qTypes":["text", "text", "email", "text", "text", "text", "text", "text", "text", "list"],
+  "list_info":{" ":"(please select a country)"},
+  "count":0
+};
+
+var education = {
+  "name":"education",
+  "id_tags":["edu_name", "edu_level", "edu_city", "edu_state", "edu_grad_date"],
+  "labels":["School* ", "Level of Education* ", "City/Town* ", "State* ", "Graduation Date* "],
+  "qTypes":["text", "list", "text", "text", "month"],
+  "list_info":{"Elementary":"Elementary",
+               "Secondary":"Secondary",
+               "Bachelors":"Bachelors",
+               "Masters":"Masters",
+               "Doctorate":"Doctorate"},
+  "count":0
+};
+
+var experience = {
+  "name":"experience",
+  "id_tags":["job_title", "job_company", "job_location", "job_start_date", "job_end_date", "job_description"],
+  "labels":["Job Title* ", "Company* ", "Job Location* ", "Job Start Date* ", "Job End Date* ", "Job Description* "],
+  "qTypes":["text", "text", "text", "month", "month", "long"],
+  "count":0
+};
+
+var activity = {
+  "name":"activity",
+  "id_tags":["act_name", "act_title", "act_location", "act_start_date", "act_end_date", "act_description"],
+  "labels":["Name of Activity* ", "Activity Title* ", "Activity Location* ", "Activity Start Date* ", "Activity End Date* ", "Activity Description* "],
+  "qTypes":["text", "text", "text", "month", "month", "long"],
+  "count":0
+};
+
 /* function to create the label for the question
  * params:
  *   section: string representing the div the question belongs to
@@ -94,46 +135,6 @@ function addListQuestion(section, qLabel, qID, options) {
 
   makeBreak(section);
 }
-
-var personal = {
-  "name":"personal",
-  "id_tags":["personal_firstname", "personal_lastname", "personal_email", "personal_number", "personal_address1", "personal_address2",
-                  "personal_city", "personal_state", "personal_zip", "personal_country"],
-  "labels":['First name* ', 'Last name* ', 'Email* ', 'Phone Number* ', 'Address Line 1* ',
-                'Address Line 2* ', 'City* ', 'State* ', 'Zip* ', 'Country* ',],
-  "qTypes":["text", "text", "email", "text", "text", "text", "text", "text", "text", "list"],
-  "list_info":{" ":"(please select a country)"},
-  "count":0
-};
-
-var education = {
-  "name":"education",
-  "id_tags":["edu_name", "edu_level", "edu_city", "edu_state", "edu_grad_date"],
-  "labels":["School* ", "Level of Education* ", "City/Town* ", "State* ", "Graduation Date* "],
-  "qTypes":["text", "list", "text", "text", "month"],
-  "list_info":{"Elementary":"Elementary",
-               "Secondary":"Secondary",
-               "Bachelors":"Bachelors",
-               "Masters":"Masters",
-               "Doctorate":"Doctorate"},
-  "count":0
-};
-
-var experience = {
-  "name":"experience",
-  "id_tags":["job_title", "job_company", "job_location", "job_start_date", "job_end_date", "job_description"],
-  "labels":["Job Title* ", "Company* ", "Job Location* ", "Job Start Date* ", "Job End Date* ", "Job Description* "],
-  "qTypes":["text", "text", "text", "month", "month", "long"],
-  "count":0
-};
-
-var activity = {
-  "name":"activity",
-  "id_tags":["act_name", "act_title", "act_location", "act_start_date", "act_end_date", "act_description"],
-  "labels":["Name of Activity* ", "Activity Title* ", "Activity Location* ", "Activity Start Date* ", "Activity End Date* ", "Activity Description* "],
-  "qTypes":["text", "text", "text", "month", "month", "long"],
-  "count":0
-};
 
 /* function to add new set of questions to div
  * params:
