c50b1e92d9d59922c0d69e2d0e4b8be3f447d437
Christina Ford <fordc@cs.uchicago.edu>
2018-08-30 21:08:49 -0400

    first round of refactoring

--- static/example.js
+++ static/example.js
@@ -19,69 +19,77 @@ function makeBreak(section) {
   document.getElementById(section).appendChild(document.createElement("BR"));
 }
 
-/* function to add questions with text input
+/* function that abstracts creating each type of question
  * params:
- *   section: string representing the div the question belongs to
- *    qLabel: string representing text that goes with the question
- *       qID: string representing the name/id of the question
+ *      section: string representing the div the question belongs to
+ *       qLabel: string representing text to display for the question
+ *          qID: string representing the name/id of the question
+ *   attributes: a dictionary containing the attributes needed for the question
  */
+function addQuestion(section, qLabel, qID, attributes) {
+  //create the label for the question
+  createTitle(section, qLabel);
+
+  //create the HTML element using Javascript
+  var inNode = document.createElement(attributes["qType"]);
+  //loop through the dictionary to apply attributes to the question
+  for (var key in attributes) {
+    inNode.setAttribute(key, attributes[key]);
+  }
+  //place element on HTML page
+  document.getElementById(section).appendChild(inNode);
+
+  //add break between questions
+  makeBreak(section);
+}
+
+/* function to add questions with text input */
 function addTextQuestion(section, qLabel, qID) {
-  createTitle(section, qLabel);
-
-  //creates text input field with name and id of `qID` and appends to div
-  var inNode = document.createElement("INPUT");
-  inNode.setAttribute("type", "text");
-  inNode.setAttribute("name", qID);
-  inNode.setAttribute("id", qID);
-  document.getElementById(section).appendChild(inNode);
-
-  makeBreak(section);
+  var attributes = {
+    "qType":"INPUT",
+    "type":"text",
+    "name":qID,
+    "id":qID
+  };
+  addQuestion(section, qLabel, qID, attributes);
 }
 
-/* function to add questions with email input
- * params:
- *   section: string representing the div the question belongs to
- *    qLabel: string representing text that goes with the question
- *       qID: string representing the name/id of the question
- */
+/* function to add questions with email input */
 function addEmailQuestion(section, qLabel, qID) {
-  createTitle(section, qLabel);
-
-  //creates text input field with name and id of `qID` and appends to div
-  var inNode = document.createElement("INPUT");
-  inNode.setAttribute("type", "email");
-  inNode.setAttribute("name", qID);
-  inNode.setAttribute("id", qID);
-  document.getElementById(section).appendChild(inNode);
-
-  makeBreak(section);
+  var attributes = {
+    "qType":"INPUT",
+    "type":"email",
+    "name":qID,
+    "id":qID
+  };
+  addQuestion(section, qLabel, qID, attributes);
 }
 
-/* function to add questions with email input
- * params:
- *   section: string representing the div the question belongs to
- *    qLabel: string representing text that goes with the question
- *       qID: string representing the name/id of the question
- */
-function addPhoneQuestion(section, qLabel, qID) {
-  createTitle(section, qLabel);
+/* function to add question with date */
+function addDateQuestion(section, qLabel, qID) {
+  var attributes = {
+    "qType":"INPUT",
+    "type":"month",
+    "name":qID,
+    "id":qID
+  };
+  addQuestion(section, qLabel, qID, attributes);
+}
 
-  //creates text input field with name and id of `qID` and appends to div
-  var inNode = document.createElement("INPUT");
-  inNode.setAttribute("type", "tel");
-  inNode.setAttribute("name", qID);
-  inNode.setAttribute("id", qID);
-  inNode.setAttribute("pattern", "[0-9]{3}-[0-9]{3}-[0-9]{4}");
-  document.getElementById(section).appendChild(inNode);
-
-  makeBreak(section);
+/* function to add long input type question  */
+function addLongQuestion(section, qLabel, qID) {
+  var attributes = {
+    "qType":"TEXTAREA",
+    "rows":4,
+    "cols":50,
+    "name":qID,
+    "id":qID
+  };
+  addQuestion(section, qLabel, qID, attributes);
 }
 
 /* function to add question with list options
  * params:
- *   section: string representing the div the question belongs to
- *    qLabel: string representing text that goes with the question
- *       qID: string representing the name/id of the question
  *   options: list representing the value/text of the options
  */
 function addListQuestion(section, qLabel, qID, options) {
@@ -105,49 +113,84 @@ function addListQuestion(section, qLabel, qID, options) {
   makeBreak(section);
 }
 
-//global variable representing how often button has been clicked
-var sectionClicked = 0;
-
-/* function to add new set of questions to div
- *
- */
-function addSection() {
-  //represents the base id string; each set of questions will have the clicks appended to it
-  //valid id: personal_firstname_1
-  var id_tags = ["personal_firstname", "personal_lastname", "personal_email", "personal_number", "personal_address1", "personal_address2",
-                  "personal_city", "personal_state", "personal_zip", "personal_country"];
-  sectionClicked++;
-
-  // //options for education levels
-  // var level_options = ["Elementary", "Secondary", "Bachelors", "Masters", "Doctorate"];
-
-  var countries = [" ", "(please select a country)", "AF", "Afghanistan",
+var personal = {
+  "name":"personal",
+  "id_tags":["personal_firstname", "personal_lastname", "personal_email", "personal_number", "personal_address1", "personal_address2",
+                  "personal_city", "personal_state", "personal_zip", "personal_country"],
+  "labels":['First name* ', 'Last name* ', 'Email* ', 'Phone Number* ', 'Address Line 1* ',
+                'Address Line 2* ', 'City* ', 'State* ', 'Zip* ', 'Country* ',],
+  "qTypes":["text", "text", "email", "text", "text", "text", "text", "text", "text", "list"],
+  "list_info":[" ", "(please select a country)", "AF", "Afghanistan",
                   "AL", "Albania", "DZ", "Algeria",
                   "AS", "American Samoa", "AD", "Andorra",
-                  "AO", "Angola", "AI", "Anguilla"];
+                  "AO", "Angola", "AI", "Anguilla"]
+};
 
-  addTextQuestion('personal', 'First name* ', id_tags[0] + "_" + sectionClicked);
-  addTextQuestion('personal', 'Last name* ', id_tags[1] + "_" + sectionClicked);
-  addEmailQuestion('personal', 'Email* ', id_tags[2] + "_" + sectionClicked);
-  addTextQuestion('personal', 'Phone Number* ', id_tags[3] + "_" + sectionClicked);
-  addTextQuestion('personal', 'Address Line 1* ', id_tags[4] + "_" + sectionClicked);
-  addTextQuestion('personal', 'Address Line 2* ', id_tags[5] + "_" + sectionClicked);
-  addTextQuestion('personal', 'City* ', id_tags[6] + "_" + sectionClicked);
-  addTextQuestion('personal', 'State* ', id_tags[7] + "_" + sectionClicked);
-  addTextQuestion('personal', 'Zip* ', id_tags[8] + "_" + sectionClicked);
-  addListQuestion('personal', 'Country* ', id_tags[9] + "_" + sectionClicked, countries);
-}
+var education = {
+  "name":"education",
+  "id_tags":["edu_name", "edu_level", "edu_city", "edu_state", "edu_grad_date"],
+  "labels":["School* ", "Level of Education* ", "City/Town* ", "State* ", "Graduation Date* "],
+  "qTypes":["text", "list", "text", "text", "month"],
+  "list_info":["Elementary", "Elementary", "Secondary", "Secondary", "Bachelors", "Bachelors", "Masters", "Masters", "Doctorate", "Doctorate"]
+};
 
-/* function used to fill data in form; used to validate ids are working properly
- *
+var experience = {
+  "name":"experience",
+  "id_tags":["job_title", "job_company", "job_location", "job_start_date", "job_end_date", "job_description"],
+  "labels":["Job Title* ", "Company* ", "Job Location* ", "Job Start Date* ", "Job End Date* ", "Job Description* "],
+  "qTypes":["text", "text", "text", "month", "month", "long"]
+};
+
+var activity = {
+  "name":"activity",
+  "id_tags":["act_name", "act_title", "act_location", "act_start_date", "act_end_date", "act_description"],
+  "labels":["Name of Activity* ", "Activity Title* ", "Activity Location* ", "Activity Start Date* ", "Activity End Date* ", "Activity Description* "],
+  "qTypes":["text", "text", "text", "month", "month", "long"]
+};
+
+/* function to add new set of questions to div
+ * params:
+ *   section: the object representing the section
  */
-function fillData() {
-  document.getElementById('personal_firstname_1').value = "John";
-  document.getElementById('personal_lastname_1').value = "Doe";
+var perCount = 0;
+var eduCount = 0;
+var expCount = 0;
+var actCount = 0;
+function addSection(section) {
+  var count;
+  if (section["name"] == "personal") {
+    count = perCount;
+  } else if (section["name"] == "education") {
+    count = eduCount++;
+  } else if (section["name"] == "experience") {
+    count = expCount++;
+  } else if (section["name"] == "activity") {
+    count = actCount++;
+  }
+  var id_tag = section["id_tags"];
+  var label = section["labels"];
+  var qType = section["qTypes"];
+  var list_info = section["list_info"];
 
-  document.getElementById('personal_firstname_2').value = "Jane";
-  document.getElementById('personal_lastname_2').value = "Doe";
-
-  document.getElementById('school_level_1').value = "Secondary";
-  document.getElementById('school_level_2').value = "Masters";
+  for (var i=0; i<qType.length; i++) {
+    if (qType[i] == "text") {
+      addTextQuestion(section["name"], label[i], id_tag[i] + "_" + count);
+    } else if (qType[i] == "email") {
+      addEmailQuestion(section["name"], label[i], id_tag[i] + "_" + count);
+    } else if (qType[i] == "list") {
+      addListQuestion(section["name"], label[i], id_tag[i] + "_" + count, list_info);
+    } else if (qType[i] == "month") {
+      addDateQuestion(section["name"], label[i], id_tag[i] + "_" + count);
+    } else if (qType[i] == "long") {
+      addLongQuestion(section["name"], label[i], id_tag[i] + "_" + count);
+    }
+  }
 }
+
+/* self-invoking function to automatically create one module for each div */
+(function () {
+  addSection(personal);
+  addSection(education);
+  addSection(experience);
+  addSection(activity);
+}());
